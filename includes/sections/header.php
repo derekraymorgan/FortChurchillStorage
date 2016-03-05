1675e828df2c1fb45a883b8b0a747a672005f69d
derekraymorgan <derek.ray.morgan@gmail.com>
2016-03-05 14:11:07 -0800

    Fixed error in 5x10 shed price

--- includes/sections/header.php
+++ includes/sections/header.php
@@ -2,9 +2,9 @@
 <header class="row">
 
 	<div class="wide">
-		<div class="col-xs-1"></div>
-		<div class="col-xs-10 logo centered-text"><h1 id="mainHeader">Fort Churchill Storage</h1></div>
-		<div class="col-xs-1"></div>
+		<div class="col-xs-0 col-sm-1"></div>
+		<div class="col-xs-12 col-sm-10 logo centered-text"><h1 id="mainHeader">Fort Churchill Storage</h1></div>
+		<div class="col-xs-0 col-sm-10"></div>
 	</div>
 
 </header>
--- payments.php
+++ payments.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 
 <html lang = "en-US">
@@ -28,6 +25,7 @@ include('includes/sections/nav.php');
 
 ?>
 
+
 <div class="row mainContent">
 
 	<h2 class="row pageTitle">Choose from the payment options below.</h2>
@@ -45,16 +43,14 @@ include('includes/sections/nav.php');
 				<p>If you would like to make a single one time payment then use the form below.</p>
 
 				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-
 					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="6YKF37EAR8NL6">
 
-					<input type="hidden" name="hosted_button_id" value="VAG2KESCYXGKC">
-
-					<input type="hidden" name="on0" value="Make a Payment">
-					<span>Make a Payment</span>
+					<input type="hidden" name="on0" value="Select storage unit size:">
+					<span>Select storage unit size:</span>
 
 					<select name="os0">
-						<option value="5x10 Storage Unit">5x10 Storage Unit $35.00 USD</option>
+						<option value="5x10 Storage Unit">5x10 Storage Unit $30.00 USD</option>
 						<option value="10x10 Storage Unit">10x10 Storage Unit $45.00 USD</option>
 						<option value="15x10 Storage Unit">15x10 Storage Unit $55.00 USD</option>
 						<option value="20x10 Storage Unit">20x10 Storage Unit $65.00 USD</option>
@@ -62,28 +58,30 @@ include('includes/sections/nav.php');
 						<option value="40x10 Storage Unit">40x10 Storage Unit $130.00 USD</option>
 						<option value="Covered RV Parking">Covered RV Parking $40.00 USD</option>
 					</select>
+
 					<br>
-					<input type="hidden" name="on1" value="Unit number and customer name">
 					<br>
 
-					<p>Enter unit number and customer name below</p>
-
+					<input type="hidden" name="on1" value="Customer Name">
+					<p>Enter customer name below:</p>
 					<input type="text" name="os1" maxlength="200">
+					<input type="hidden" name="on2" value="Storage Unit Number">
+					<p>Enter storage unit number below:</p>
+					<input type="text" name="os2" maxlength="200">
+					<br>
+
 
 					<input type="hidden" name="currency_code" value="USD">
 
 					<br>
-					<br>
 
 					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-
 					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 				</form>
 
 			</div>
 
 			<div class="col-xs-1"></div>
-
 
 		</div>
 
@@ -97,16 +95,17 @@ include('includes/sections/nav.php');
 
 				<p>If you would like to sign up for recurring monthly payments then use the form below.</p>
 
+
 				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="ND6HDFL9JNFUJ">
+					<input type="hidden" name="hosted_button_id" value="XS7ZR9U522MLG">
 
-					<input type="hidden" name="on0" value="Monthly Recurring Payments">
+					<input type="hidden" name="on0" value="Select storage unit size:">
 
-					<span>Monthly Recurring Payments</span>
+					<span>Select storage unit size:</span>
 
 					<select name="os0">
-						<option value="5x10 Storage Unit">5x10 Storage Unit : $35.00 USD - monthly</option>
+						<option value="5x10 Storage Unit">5x10 Storage Unit : $30.00 USD - monthly</option>
 						<option value="10x10 Storage Unit">10x10 Storage Unit : $45.00 USD - monthly</option>
 						<option value="15x10 Storage Unit">15x10 Storage Unit : $55.00 USD - monthly</option>
 						<option value="20x10 Storage Unit">20x10 Storage Unit : $65.00 USD - monthly</option>
@@ -117,12 +116,19 @@ include('includes/sections/nav.php');
 
 					<br>
 
-					<input type="hidden" name="on1" value="Unit number and customer name">
+					<input type="hidden" name="on1" value="Customer Name">
 
 					<br>
-					<p>Enter unit number and customer name below</p>
+					<p>Enter customer name below</p>
 
 					<input type="text" name="os1" maxlength="200">
+
+					<input type="hidden" name="on2" value="Storage Unit Number">
+
+					<br>
+					<p>Enter storage unit number below:</p>
+
+					<input type="text" name="os2" maxlength="200">
 
 					<input type="hidden" name="currency_code" value="USD">
 
@@ -130,7 +136,6 @@ include('includes/sections/nav.php');
 					<br>
 
 					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-
 					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 
 				</form>
@@ -157,10 +162,6 @@ include('includes/sections/nav.php');
 </body>
 
 </html>
-
-
-
-
 
 
 
